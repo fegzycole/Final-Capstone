78b0747f6e34b01db89f0504d0898f3774e991ff
FERGUSON IYARA <fergusoniyara@gmail.com>
2020-06-07 09:49:57 +0100

    Complete user registration and login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,15 +5,24 @@ module Api
         @user = User.new(user_params)
 
         if @user.save
-          session[:user_id] = @user.id
-
-          data = { id: session[:user_id], firstName: @user.first_name,
-                   lastName: @user.last_name, email: @user.email }
+          data = get_data(@user)
 
           json_response(data, :ok)
         else
           json_response({ errors: @user.errors.full_messages },
                         :unprocessable_entity)
+        end
+      end
+
+      def login
+        @user = User.find_by_email(params[:email])
+
+        if @user&.authenticate(params[:password])
+          data = get_data(@user)
+
+          json_response(data, :ok)
+        else
+          json_response({ error: 'unauthorized' }, :unauthorized)
         end
       end
 
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,4 +2,15 @@ module Response
   def json_response(object, status = :ok)
     render json: object, status: status
   end
+
+  def get_data(user_data)
+    user = { id: user_data.id, firstName: user_data.first_name,
+             lastName: user_data.last_name, email: user_data.email }
+
+    token = JsonWebToken.encode(user: user)
+
+    time = Time.now + 24.hours.to_i
+
+    { token: token, exp: time.strftime('%m-%d-%Y %H:%M')}
+  end
 end
